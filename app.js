d8c778aa5d82ce37381e5bab9568b8cb876591be
leralys <valerialysko@gmail.com>
2022-01-21 18:05:55 +0200

    forms, registration

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 // Imports
-const express = require('express');
-const env = require('dotenv');
+const bcrypt = require('bcrypt');
 const cors = require('cors');
-const path = require('path');
+const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const env = require('dotenv');
+const path = require('path');
 
 const app = express();
 
+
+// --- mocking user table in db ---
+const users = [];
+// --- end ---
+
 env.config();
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
 // Set templating engine
 app.use(expressLayouts);
-app.set('layout', './layouts/full')
+app.set('layout', './layouts/nobackground');
+// Set view engine
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
@@ -36,9 +44,60 @@ app.get('/api/products', (req, res) => {
 
 
 // Navigation
+//root
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
+//shop
+app.get('/products', (req, res) => {
+    res.render('index');
+    //contact form 
+});
+app.get('/contact', (req, res) => {
+    res.render('index');
+});
+//about
+app.get('/about', (req, res) => {
+    res.render('index');
+});
+//register
+app.get('/register', (req, res) => {
+    res.render('register', { title: 'Register', layout: './layouts/background' });
+});
+app.post('/register', (req, res) => { //async
+    const saltRounds = 10;
+    bcrypt.hash(req.body.password, saltRounds).then(function (hash) {
+        // Store hash in your password DB
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hash
+        });
+        console.log(users);
+    })
+        .catch(e => console.log(e));
+
+    res.redirect('/register');
+});
+// try {
+//     const hashedPass = await bcrypt.hash(req.body.password, saltRounds);
+//     users.push({
+//         id: Date.now().toString(),
+//         name: req.body.name,
+//         email: req.body.email,
+//         password: hashedPass
+//     })
+//     res.redirect('/login');
+// } catch {
+//     res.redirect('/register');
+// }
+
+
+//login
+app.get('/login', (req, res) => {
+    res.render('login', { title: 'Login', layout: './layouts/background' });
+});
 
 
 
